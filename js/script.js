9a085e5c44714ec85a270edb2e2a89e191682d95
MikaHacks <93355652+MikaHacks@users.noreply.github.com>
2022-04-08 08:19:08 -0400

    Batch update

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@
 
     window.silex = window.silex || {}
     window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-page-1","displayName":"Page 1","link":{"linkType":"LinkTypePage","href":"#!page-page-1"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Javascripts</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="simplecolour.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../sheep2.css">
+    <script src="../sheep2.js" charset="utf-8">
